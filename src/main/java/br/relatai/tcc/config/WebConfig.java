689bfa0d49d32c9e8116194dc085271c3fc4f018
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2018-03-19 13:53:47 -0300

    more configurations to enable CORS on ionic http delete

--- src/main/java/br/relatai/tcc/config/WebConfig.java
+++ src/main/java/br/relatai/tcc/config/WebConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig extends WebMvcConfigurerAdapter{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/**")
+        	.allowedOrigins("*")
+        	.allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS")
+        	.allowedHeaders("X-Requested-With,Content-Type,Accept,Origin");
 	}
 }
